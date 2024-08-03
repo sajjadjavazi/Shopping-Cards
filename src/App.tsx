86e6966b96a378626be58a1c97865c104d07c98c
Sajjad <sajjad.javazi.01@gmail.com>
2024-08-03 22:06:58 +0330

    name changed

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,19 @@ function App() {
     <Layout>
       <div className="container mx-auto p-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-        <Post title="rolls roys culis" description='made by mansory' price={2.5} pricebadge='Mil'
+        <Post title="Rolls-Royce Cullinan" description='made by mansory' price={2.5} pricebadge='Mil'
         />
-        <Post title="rolls roys culis" description='made by mansory' price={2.5} pricebadge='Mil'
+        <Post title="Rolls-Royce Cullinan" description='made by mansory' price={2.5} pricebadge='Mil'
         />
-        <Post title="rolls roys culis" description='made by mansory' price={2.5} pricebadge='Mil'
+        <Post title="Rolls-Royce Cullinan" description='made by mansory' price={2.5} pricebadge='Mil'
         />
-        <Post title="rolls roys culis" description='made by mansory' price={2.5} pricebadge='Mil'
+        <Post title="Rolls-Royce Cullinan" description='made by mansory' price={2.5} pricebadge='Mil'
         />
-        <Post title="rolls roys culis" description='made by mansory' price={2.5} pricebadge='Mil'
+        <Post title="Rolls-Royce Cullinan" description='made by mansory' price={2.5} pricebadge='Mil'
         />
-        <Post title="rolls roys culis" description='made by mansory' price={2.5} pricebadge='Mil'
+        <Post title="Rolls-Royce Cullinan" description='made by mansory' price={2.5} pricebadge='Mil'
         />
-        <Post title="rolls roys culis" description='made by mansory' price={2.5} pricebadge='Mil'
+        <Post title="Rolls-Royce Cullinan" description='made by mansory' price={2.5} pricebadge='Mil'
         />
       </div>
     </div>
