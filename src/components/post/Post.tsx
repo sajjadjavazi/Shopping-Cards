abc7ca26b527d6bf6363dca06854a2a3626d4f57
Sajjad <sajjad.javazi.01@gmail.com>
2024-08-03 22:55:06 +0330

    refactor(post): update component to use props

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -2,22 +2,20 @@ type Post = {
     title: string,
     description: string,
     pricebadge: string,
-    price: number
+    price: number,
+    image: string
 }
 
-const Post = ({ title, description, price, pricebadge }: Post) => {
+const Post = ({ title, description, price, pricebadge, image }: Post) => {
     return (
-        <div>
-            {/* <!-- Card 1 --> */}
-            <div className="bg-white shadow-md rounded-lg overflow-hidden">
-                <img src="./src/mansory-rolls-royce-cullinan-special-uae-front-corner.jpg" alt="Rolls-Royce Cullinan" className="w-full h-48 object-cover" />
-                <div className="p-4">
-                    <h3 className="text-lg font-semibold">{title}</h3>
-                    <p className="text-gray-600">{description}</p>
-                    <div className="mt-4 flex justify-between items-center">
-                        <span className="text-xl font-bold">{price}{pricebadge}</span>
-                        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Contact Us</button>
-                    </div>
+        <div className="max-w-sm mx-auto bg-gray-50 shadow-lg rounded-lg overflow-hidden transform transition-transform hover:scale-105">
+            <img src={image} alt={title} className="w-full h-52 object-cover" />
+            <div className="p-5">
+                <h3 className="text-xl font-semibold text-gray-800">{title}</h3>
+                <p className="text-gray-600 mt-2">{description}</p>
+                <div className="mt-4 flex justify-between items-center">
+                    <span className="text-2xl font-bold text-gray-800">{price}{pricebadge}</span>
+                    <button className="bg-green-500 text-white px-4 py-2 rounded-full hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2">Contact Us</button>
                 </div>
             </div>
         </div>
