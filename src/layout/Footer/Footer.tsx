3a102216d96d11639108c9af752e815d3bdb70e0
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-08-03 21:27:30 +0330

    initial commit

---
+++ src/layout/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="bg-orange-500 w-full h-12 rounded-xl flex justify-center items-center">
+            <h1>End of the site!</h1>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/layout/Header/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div>
+            <nav className="bg-blue-500 w-full h-16 flex items-center justify-center rounded-bl-3xl rounded-br-3xl">
+                <h1 className="text-xl font-bold"><a href="https://en.wikipedia.org/wiki/Mansory" target="_blank">Mansory</a> luxury car shop</h1>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
